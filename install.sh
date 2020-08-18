e44cab1309467cf911fdbbe569a466652c1af813
thaoeu <154942630@qq.com>
2020-08-18 08:00:21 +0800

    更改任务栏背景色

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ cp -r $PWD/rime ~/.config/fcitx
 echo 'export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 export XMODIFIERS="@im=fcitx"' >> /etc/X11/xinit/xinitrc
+cp ./bar.png /usr/share/fcitx/skin/classic
 fcitx-configtool
