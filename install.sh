57d6b486cb423cffc565f81198564c837938d474
thaoeu <154942630@qq.com>
2020-09-27 15:28:38 +0800

    修复 fcitx 的路径配置错误

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 pacman -S --noconfirm fcitx fcitx-im fcitx-rime fcitx-configtool
 mkdir -p ~/.config/fcitx
 cp -r $PWD/rime ~/.config/fcitx
-echo 'export GTK_IM_MODULE=fcitx
+echo '
+# fcitx
+export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
-export XMODIFIERS="@im=fcitx"' >> /etc/X11/xinit/xinitrc
+export XMODIFIERS="@im=fcitx"
+' >> /etc/profile
 cp ./bar.png /usr/share/fcitx/skin/classic
 #fcitx-configtool
 ln -f $PWD/rime/flypy_user.txt ~/.config/fcitx/rime
